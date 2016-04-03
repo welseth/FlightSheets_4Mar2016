f6e53f626676fad1bb487d2bd295c69a44e43473
wayne.elseth <wayne.elseth@outlook.com>
2016-04-03 12:11:00 -0400

    NOT done, zeroing out some unused TowPilot2/3 values in the DB, trying to get them to be "blank" instead of an actual number.

--- MASA_redo_3Apr2016/MASA_redo_3Apr2016/Form1.vb
+++ MASA_redo_3Apr2016/MASA_redo_3Apr2016/Form1.vb
@@ -99,18 +99,18 @@
         newFlightRow.AOD1 = OD_AOD_AOD1_ComboBox.SelectedIndex
         newFlightRow.AOD2 = OD_AOD_AOD2_ComboBox.SelectedIndex
         newFlightRow.TowPilot1 = TowPilotNameComboBox.SelectedIndex
-        newFlightRow.TowPilot2 = TowPilotNameComboBox.SelectedIndex  'towpilot2 not used, but assigning anyway
-        newFlightRow.TowPilot3 = TowPilotNameComboBox.SelectedIndex  'towpilot3 not used, but assigning anyway
+        newFlightRow.TowPilot2 = vbNull 'towpilot2 not used, but assigning anyway
+        newFlightRow.TowPilot3 = vbNull  'towpilot3 not used, but assigning anyway
         newFlightRow.TowPlane1 = TowPlaneComboBox.SelectedIndex
-        newFlightRow.TowPlane2 = TowPlaneComboBox.SelectedIndex  'towplane2 not used, but assigning anyway
+        newFlightRow.TowPlane2 = vbNull  'towplane2 not used, but assigning anyway
         Debug.Print("Todays DateTimePicker1: " & Todays_Date_DateTimePicker.Value)
         'MessageBox.Show("DateTimePicker:  " & DateTimePicker1.Value)  'get rid of this after debugging is complete 
         Debug.Print("Glider Takeoff:  " & TakeOff_DateTimePicker.Value)
         newFlightRow.Glider_takeoff_time = TakeOff_DateTimePicker.Value
         Debug.Print("Glider Landing:  " & Landing_DateTimePicker.Value)
         newFlightRow.Glider_landing_time = Landing_DateTimePicker.Value
-        newFlightRow.Tow_takeoff_time = TakeOff_DateTimePicker.Value
-        newFlightRow.Tow_landing_time = Landing_DateTimePicker.Value
+        newFlightRow.Tow_takeoff_time = CType(Nothing, DateTime)   'TowTakeoffTime not used, just setting it to Nothing
+        newFlightRow.Tow_landing_time = CType(Nothing, DateTime)   'TowLandingTime not used, just setting it to Nothing
         newFlightRow._Date = Todays_Date_DateTimePicker.Value  'saves in format DateTime 
 
         newFlightRow.Rope_break = RopeBreakCheckBox.Checked
